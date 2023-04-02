169236a7a3d9687e3d15ed622dd780d650643180
Moses <lazaromoses77@gmail.com>
2023-04-02 22:27:34 +0300

    adding routes and api endpints

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/goals', (req, res) => {
+    res.status(200).json({msg: "Get all Goals"})
+})
+router.get('/api/goals', (req, res) => {
+    res.status(200).json({msg: "Get all Goals"})
+})
+router.get('/api/goals', (req, res) => {
+    res.status(200).json({msg: "Get all Goals"})
+})
+router.get('/api/goals', (req, res) => {
+    res.status(200).json({msg: "Get all Goals"})
+})
+
+
+module.exports = router;
